5064902a0018fa166b4339307d94e979842b72c0
임재원 <59534382+nemo0824@users.noreply.github.com>
2023-07-26 21:14:01 +0900

    dd
    dd

--- shoes/src/App.js
+++ shoes/src/App.js
@@ -1,26 +1,86 @@
 import logo from './logo.svg';
 import './App.css';
 import { Button, Navbar,Nav, Container } from 'react-bootstrap';
+import data from './data';
+import { useState } from 'react';
+import {Routes, Route, Link, useNavigate, Outlet} from 'react-router-dom'
+import Detail from './routes/Detail'
+
 
 function App() {
+
+  let [shoes]  = useState(data)
+  let navigate = useNavigate(); // 페이지 이동을 도와주는 navigate()
+  
+  
   return (
     <div className="App">
+     
       <Navbar bg="dark" data-bs-theme="dark">
         <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Navbar.Brand href="#home">임재원</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Shop</Nav.Link>
-            <Nav.Link href="#pricing">Cart</Nav.Link>
+            <Nav.Link onClick={()=> navigate('/')}>home</Nav.Link>
+            <Nav.Link onClick={()=> navigate('/shop')}>Shop</Nav.Link>
+            <Nav.Link onClick={()=> navigate('/cart')}>Cart</Nav.Link>
+            <Nav.Link onClick={()=> navigate('/detail')}>Detail</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
-      <div className='main-bg'>
-        
-      </div>
 
+
+      <Routes>
+      <Route path='/' element={
+          <>
+            <div className='main-bg'></div>
+            <div className="container">
+              <div className="row">
+                {
+                  shoes.map((a, i) => {
+                    return (
+                      <Card shoes={shoes[i]} i={i + 1}></Card>
+                    )
+                  })
+                }
+              </div>
+            </div>
+          </>
+      }>
+
+      </Route>
+        <Route path='/shop'></Route>
+        <Route path='/detail/:id' element={<Detail shoes={shoes}></Detail>}></Route>
+        <Route path='/cart'></Route>
+        <Route path='*' element={<div> 404에러 없는페이지 </div>}></Route>
+        
+        <Route></Route>
+      </Routes>
+
+
+      
     </div>
   );
 }
 
+
+
+function Card(props){
+  return(
+    <div className="col-md-4">
+    <img src={process.env.PUBLIC_URL + '/nikeshoes'+ props.i + '.jpg'}width="80%" height="60%"/>
+    <h4>{props.shoes.title}</h4>
+    <p>{props.shoes.content} </p>
+    <p>{props.shoes.price} 원</p>
+  </div>
+  )
+}
+//nested routes 경로/경로  이단경로 이상 
+
+function About(){
+  return(
+    <div>
+      <h4>회사정보</h4>
+    </div>
+  )
+}
 export default App;
---
+++ shoes/src/data.js
@@ -0,0 +1,22 @@
+let data = [
+{
+    id: 0,
+    title : "나이키 코르테즈",
+    content : "DM4044-100",
+    price : 119000
+},
+{
+    id: 1,
+    title : "나이키 에어 포스 1 '07",
+    content : "CW2288-111",
+    price : 139000
+},
+{
+    id: 1,
+    title : "나이키 인빈서블 3 SE",
+    content : "FJ1046-100",
+    price : 209000
+}
+]
+
+export default data;
