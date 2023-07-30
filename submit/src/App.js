fce05570bb9fa7fc49da0aa0041a59699449d5de
임재원 <59534382+nemo0824@users.noreply.github.com>
2023-07-31 02:14:29 +0900

    dd
    dd

--- submit/src/App.js
+++ submit/src/App.js
@@ -2,15 +2,17 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Button, Navbar, Container, Nav} from 'react-bootstrap'
 import React, { useState } from 'react';
-import './TodaySchedule'
-import TodaySchedule from './TodaySchedule';
+import TextExample from './TodaySchedule';
+import Profile from './Profile';
 
 function App() {
+  
+
   return (
-    <div>
+    <div className='full'>
       <Navbar bg="dark" variant="dark">
         <Container>
-        <Navbar.Brand href="#home">Care square</Navbar.Brand>
+        <Navbar.Brand href="#home">이력서</Navbar.Brand>
         <Nav className="me-auto">
           <Nav.Link href="#home">지원자 임재원</Nav.Link>
           <Nav.Link href="#features">daily</Nav.Link>
@@ -18,7 +20,15 @@ function App() {
         </Nav>
         </Container>
       </Navbar>
-      <TodaySchedule></TodaySchedule>
+
+      <div className='bg-img'>
+      <div className='bg-img-real'></div>
+      </div>
+
+      <div className='layout'>
+      <Profile/>
+      <TextExample/>
+      </div>
      
 
     </div>
