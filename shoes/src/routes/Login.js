4e0a6c2fd45a0245de2d42c21ccf256089622bf2
임재원 <59534382+nemo0824@users.noreply.github.com>
2023-07-31 15:46:18 +0900

    Create Login.js
    대소문자 문제관련

---
+++ shoes/src/routes/Login.js
@@ -0,0 +1,26 @@
+import { Container } from 'react-bootstrap';
+import Form from 'react-bootstrap/Form';
+
+function Login() {
+  return (
+    <div className=''>
+    <Container className="d-flex justify-content-center align-items-center" style={{ height: '100vh' }}>
+    <Form style={{ maxWidth: '300px' }}>
+      <Form.Group className="mb-3" controlId="formGroupEmail">
+        <Form.Label>Email </Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formGroupPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+        <button>로그인 버튼</button>
+      </Form.Group>
+    </Form>
+    </Container>
+    </div>
+    
+    
+  );
+}
+
+export default Login;
