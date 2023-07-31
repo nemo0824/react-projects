69f6037778e855bacdadd207d27aaa529bd13505
임재원 <59534382+nemo0824@users.noreply.github.com>
2023-07-31 22:06:10 +0900

    submit
    submit

--- submit/src/App.js
+++ submit/src/App.js
@@ -5,6 +5,8 @@ import React, { useState } from 'react';
 import TextExample from './TodaySchedule';
 import Profile from './Profile';
 import {Routes, Route, Link} from 'react-router-dom'
+import TabIntroduce from './TabIntroduce';
+
 function App() {
   
 
@@ -14,11 +16,11 @@ function App() {
      
       <Navbar bg="dark" variant="dark">
         <Container>
-        <Navbar.Brand href="#home">이력서</Navbar.Brand>
+        <Navbar.Brand href="/">이력서</Navbar.Brand>
         <Nav className="me-auto">
-          <Nav.Link href="#home">지원자 임재원</Nav.Link>
-          <Nav.Link href="#features">취미</Nav.Link>
-          <Nav.Link href="#pricing">자기소개서</Nav.Link>
+          <Nav.Link href="#">지원자 임재원</Nav.Link>
+          <Nav.Link href="/hobby">취미</Nav.Link>
+          <Nav.Link href="#introduce">자기소개서</Nav.Link>
         </Nav>
         </Container>
       </Navbar>
@@ -30,10 +32,13 @@ function App() {
         <Profile/>
       <TextExample/>
       </div> 
-      
+      <TabIntroduce></TabIntroduce>
+
       <Routes>
       <Route path="/" elemnet={<div>메인</div>}></Route>
         <Route path="/hobby" elemnet={<div>취미</div>}></Route>
+        <Route path="/introduce" elemnet={<div>취미</div>}></Route>
+        <Route path="/introduce" elemnet={<div>취미</div>}></Route>
       </Routes>
 
       
