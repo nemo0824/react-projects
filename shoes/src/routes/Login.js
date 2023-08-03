002e500c802e29ee175ab7b98aa6a4ce5accc5c3
임재원 <nemo0824@naver.com>
2023-08-04 00:42:31 +0900

    login,enroll form
    login,enroll form

--- shoes/src/routes/Login.js
+++ shoes/src/routes/Login.js
@@ -7,10 +7,17 @@ function Login() {
     <div className='Login_form'> 
       <form action=''>
         <h2 className='Login_title'>Shoe Market</h2>
+        <h3> 로그인 </h3>
+        <p className='enroll_list'>아이디</p>
         <input type='text' placeholder='아이디 ' className='input'></input>
+        <p className='enroll_list'>비밀번호</p>
         <input type='password' placeholder='비밀번호 ' className='input'></input>
         <input type="submit" value="로그인" className='btn-submit'></input>
-        <a>회원가입 </a><a>|  비밀번호찾기</a> <a>|  아이디찾기</a>
+        <div className='login_atage'>
+         <a>회원가입 </a>
+         <a>비밀번호찾기</a>
+         <a>아이디찾기</a>
+        </div>
       </form>
     </div>
     </div>
