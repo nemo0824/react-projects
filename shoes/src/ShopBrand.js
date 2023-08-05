b5a91e341f5e16e6b79186bd2df41ae884c0eb66
임재원 <nemo0824@naver.com>
2023-08-06 01:59:03 +0900

    shop
    shop

---
+++ shoes/src/ShopBrand.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function ShopBrand(props){
+    return(
+        <>
+         <ul>
+         <li>{props.brand}</li>
+         </ul>
+        </>
+    )
+
+}
+
+
+export default ShopBrand;
--- shoes/src/routes/Shop.js
+++ shoes/src/routes/Shop.js
@@ -1,31 +1,46 @@
 import axios from 'axios';
 import { useState, useEffect } from 'react';
 import ShopCard from '../ShopCard';
+import ShopBrand from '../ShopBrand';
 
 function Shop() {
   const [items, setItems] = useState([]);
+  const [brand , setBrand] = useState([]);
+  
 
   useEffect(() => {
     axios.get('/item') 
       .then(response => {
-        setItems(response.data);
-        
+        setItems(response.data.item);
+        setBrand(response.data.brand);
+        console.log(response.data)
       })
       .catch(error => console.log(error));
   }, []);
 
-
+   
   return (
     <>
         
       <div className='shop_bg'></div>
       <div className="content">
         <div className='left_content'>
-          {/* 왼쪽 사이드바 */}
-          <div>나이키</div>
-          <div>뉴발란스</div>
-          <div>아디다스</div>
-          <div>컨버스</div>
+            
+          
+        {
+            brand.map((brand,i)=>(
+                <ShopBrand brand={brand} key={i} onClick={()=>{}}></ShopBrand>
+                //button onClick 하고 눌렀을때 --> 해당 브랜드 상품만나와야한다 
+                // 1. axios로  item에서 특정브랜드 가져오는  brand = brand
+                // 
+            ))
+        }
+          
+                
+
+           
+            
+            
         </div>
         <div className='right_content'>
           <div className='row'>
