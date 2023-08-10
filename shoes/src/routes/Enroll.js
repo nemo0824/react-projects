974f8d0e6bf070cda18e62f3cce5d950cc46cdb5
임재원 <nemo0824@naver.com>
2023-08-10 22:59:13 +0900

    test
    test

--- shoes/src/routes/Enroll.js
+++ shoes/src/routes/Enroll.js
@@ -4,16 +4,23 @@ import { useState, useEffect } from 'react'
 import axios from 'axios';
 
 function Enroll() {
-  // 값들 저장한 객체
-  const [form, setForm] = useState({
-    userName : '',
-    userId : '',
-    userPassword : '',
-    userPasswordCheck: '',
-    userEmail : '',
-    userPhone: '',
-  })
   
+  //객체 저장
+  const [userName , setuserName] = useState("");
+  const [userId, setuserId] = useState("");
+  const [userPassword, setuserPassword] = useState("");
+  const [userPasswordCheck, setuserPasswordCheck] = useState("");
+  const [userEmail, setuserEmail] = useState("");
+  const [userPhone, setuserPhone] = useState("");
+  
+  // const [form, setForm] = useState({
+  //   userName : "",
+  //   userId : "",
+  //   userPassword : "",
+  //   userPasswordCheck : "",
+  //   userEmail : "",
+  //   userPhone : "",
+  // })
 
   // 오류메시지
   const [nameMessage, setNameMessage] = useState("");
@@ -24,89 +31,102 @@ function Enroll() {
   const [phoneMessage, setPhoneMessage] = useState("");
 
   //유효성 검사
-  const [isName, setName] = useState(false); 
-  const [isId, setId] = useState(false);
-  const [isPassword, setPassword] = useState(false);
-  const [isPasswordCheck, setPasswordCheck] = useState(false);
-  const [isEmail, setEmail] = useState(false);
-  const [isPhone, setPhone] = useState(false);
+  const [isName, setisName] = useState(false); 
+  const [isId, setisId] = useState(false);
+  const [isPassword, setisPassword] = useState(false);
+  const [isPasswordCheck, setisPasswordCheck] = useState(false);
+  const [isEmail, setisEmail] = useState(false);
+  const [isPhone, setisPhone] = useState(false);
   
   const [messageStyle, setMessageStyle] = useState({});
 
   const onChangeName = (e) => {
     const currentName = e.target.value;
-    setForm(currentName);
+    setuserName(currentName);
     const idRegExp = /^[a-zA-z0-9]{1,5}$/;
  
     if (!idRegExp.test(currentName)) {
       setNameMessage("5글자 이하로 입력해주세요");
       setMessageStyle({ color: 'red' });
-      setName(false);
+      setisName(false);
     } else {
       setNameMessage("올바른 이름입니다 .");
       setMessageStyle({ color: 'green' });
-      setName(true);
+      setisName(true);
     }
   };
 
   const onChangeId = (e) => {
     const currentId = e.target.value;
-    setForm(currentId);
+    setuserId(currentId);
     const idRegExp = /^[a-zA-z0-9]{4,12}$/;
  
     if (!idRegExp.test(currentId)) {
       setIdMessage("4-12사이 대소문자 또는 숫자만 입력해 주세요!");
       setMessageStyle({ color: 'red' });
-      setId(false);
+      setisId(false);
     } else {
       setIdMessage("사용가능한 아이디 입니다.");
       setMessageStyle({ color: 'green' });
-      setId(true);
+      setisId(true);
     }
   };
 
   const onChangePassword = (e)=>{
     const currentPassword = e.target.value    
-    setForm(currentPassword);
+    setuserPassword(currentPassword);
     const passwordRegExp = /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{7,25}$/;
   if(!passwordRegExp.test(currentPassword)) {
     setPasswordMessage("숫자 + 영문자 + 특수문자으로 7자리 이상 입력해주세요~")
-    setPassword(false);
+    setisPassword(false);
   } else{
     setPasswordMessage("안전한 비밀번호입니다 !")
-    setPassword(true);
+    setisPassword(true);
   }
 }
+  const onChangePasswordCheck = (e)=>{
+    const currentPasswordCheck = e.target.value
+    setuserPasswordCheck(currentPasswordCheck);
+    if(userPassword!==currentPasswordCheck){
+      setPasswordCheckMessage("비밀번호가 동일하지 않습니다")
+      setisPasswordCheck(false);
+    }else{
+      setPasswordCheckMessage("비밀번호가 동일합니다")
+      setisPasswordCheck(true);
+    }
+  }
+
+
   const onChangePhone = (e) =>{
     const currentPhone = e.target.value
-    setFrom(currentPhone);
+    setuserPhone(currentPhone);
     const phoneRegExp = /^010\d{8}$/;
     if(!phoneRegExp.test(currentPhone)){
       setPhoneMessage("8자리 숫자만 입력해주세요")
-      setPhone(false)
+      setisPhone(false)
     }else{
       setPhoneMessage("옳은 전화번호 입니다")
-      setPhone(true);
+      setisPhone(true);
     }
     
   }
     const onChangeEmail = (e) =>{
       const currentEmail = e.target.value
-      setForm(currentEmail);
+      setuserEmail(currentEmail);
       const emailRegxp = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
       if(!emailRegxp.test(currentEmail)){
         setEmailMessage("이메일 형식이 옳지 않습니다 ")
-        setEmail(false)
+        setisEmail(false)
       }else{
         setEmailMessage("이메일 형식이 옳습니다")
-        setEmail(true)
+        setisEmail(true)
       }
     }
 
 
   const register = ()=>{
     axios.post('/member/enroll',
-      form
+    userName, userEmail, userId, userPassword, userPasswordCheck, userPhone
     ) 
       .then(response => {
         
@@ -123,20 +143,23 @@ function Enroll() {
         <h2 className='Login_title'>Shoe Market</h2>
         <h3> 회원가입 </h3>
         <p className='enroll_list'>이름</p>
-        <input type='text' placeholder='이름 ' className='input' value={form.userName} onChange={onChangeName}></input>
+        <input type='text' placeholder='이름 ' className='input' value={userName} onChange={onChangeName}></input>
         <p className='alertMessage' style={messageStyle}>{nameMessage}</p>
         <p className='enroll_list'>아이디</p>
-        <input type='text' placeholder='아이디 ' className='input' value={form.userId} onChange={onChangeId}></input>
+        <input type='text' placeholder='아이디 ' className='input' value={userId} onChange={onChangeId}></input>
         <p className='alertMessage' style={messageStyle}>{idMessage}</p>
         <p className='enroll_list'>비밀번호</p>
-        <input type='password' placeholder='비밀번호 ' className='input' value={form.userPassword} onChange={onChangePassword}></input>
+        <input type='password' placeholder='비밀번호 ' className='input' value={userPassword} onChange={onChangePassword}></input>
+        <p className='alertMessage' style={messageStyle}>{passwordMessage}</p>
         <p className='enroll_list'>비밀번호 확인</p>
-        <input type='password' placeholder='비밀번호 확인' className='input'value={form.userPasswordCheck} onChange={e => setForm({...form , userPasswordCheck: e.target.value})}></input>
+        <input type='password' placeholder='비밀번호 확인' className='input'value={userPasswordCheck} onChange={onChangePasswordCheck}></input>
+        <p className='alertMessage' style={messageStyle}>{passwordCheckMessage}</p>
         <p className='enroll_list'>Email</p>
-        <input type='text' placeholder='email ' className='input'value={form.userEmail} onChange={e => setForm({...form , userEmail: e.target.value})}></input>
+        <input type='text' placeholder='email ' className='input'value={userEmail} onChange={onChangeEmail}></input>
+        <p className='alertMessage' style={messageStyle}>{emailMessage}</p>
         <p className='enroll_list'>Phone</p>
-        <input type='text' placeholder='Phone ' className='input' value={form.userPhone} onChange={e => setForm({...form , userPhone: e.target.value})}></input>
-        
+        <input type='text' placeholder='Phone ' className='input' value={userPhone} onChange={onChangePhone}></input>
+        <p className='alertMessage' style={messageStyle}>{phoneMessage}</p>
         <input type="submit" value="회원가입" className='btn-submit' onClick={register}></input>
        
       </form>
