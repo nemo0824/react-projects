80a7adc5bd5ea912ca9b230b7ac2b16b0ca5dff6
임재원 <nemo0824@naver.com>
2024-05-12 22:29:38 +0900

    useEffect

--- react_code/src/App.js
+++ react_code/src/App.js
@@ -4,18 +4,39 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [keyword, setKeyword] = useState("");
   const onClick = () => setCount((prev) => prev + 1);
+  const onChange = (event) => {
+    setKeyword(event.target.value);
+  };
   console.log("all the time");
 
-  const iRunOnlyOnce = () => {
-    console.log("Only Once");
-  };
+  useEffect(() => {
+    console.log(" I Run Only once");
+  }, []);
 
   useEffect(() => {
-    console.log("api");
+    if (keyword !== "" && keyword.length > 5) {
+      console.log("Search For ", keyword);
+    }
+  }, [keyword]);
+
+  useEffect(() => {
+    console.log("I Run only Count Change");
+  }, [count]);
+
+  useEffect(() => {
+    console.log("1번만 시작때만");
   }, []);
+
   return (
     <div>
+      <input
+        type="text"
+        value={keyword}
+        placeholder="Search here"
+        onChange={onChange}
+      ></input>
       <h1 className={styles.title}>{count} </h1>
       <button onClick={onClick}>Click me </button>
       <Button text={"continue"} />
