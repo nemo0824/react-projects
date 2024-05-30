07882e2ccfe593b1cce74320b0d1fe7203fb25a3
임재원 <nemo0824@naver.com>
2024-05-30 23:39:54 +0900

    typescript 똑같이 interface, props, styled component

--- react-master/src/App.tsx
+++ react-master/src/App.tsx
@@ -1,12 +1,13 @@
 import styled, {keyframes}  from "styled-components"
 import React from 'react'
 import Circle from "./Circle";
-
+import Square from "./Square";
 
 function App() {
   return <>
     <Circle bgColor="teal"/>
     <Circle bgColor="tomato"/>
+    <Square bgColor="blue"/>
     </>
 }
 
