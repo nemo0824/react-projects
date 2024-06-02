fa981179393e58da16fdc6023aa39efab33e145c
임재원 <nemo0824@naver.com>
2024-06-03 00:16:40 +0900

    react-router-dom @6.4
    screens/about, home
    components/header 		Link to
    src/Router
    <BrowserRouter> <Routes> <Route>

--- react-master/src/App.tsx
+++ react-master/src/App.tsx
@@ -3,49 +3,44 @@ import React, { useState } from 'react'
 import Circle from "./Circle";
 import Square from "./Square";
 import { text } from "stream/consumers";
+import Router from "./Router";
+
+// interface DummyProps{
+//   text:string;
+//   active? : boolean;
+
+// }
 
 
-interface DummyProps{
-  text:string;
-  active? : boolean;
-
-}
-
-
-function Dummy({text, active = false}:DummyProps){
-  return <h1>{text}</h1>
-}
+// function Dummy({text, active = false}:DummyProps){
+//   return <h1>{text}</h1>
+// }
 
 
 function App() {
-  const [username, setUsername] = useState("")
-  const onChange = (event: React.FormEvent<HTMLInputElement>)=>{
-   const{
-    currentTarget: {value},
+//   const [username, setUsername] = useState("")
+//   const onChange = (event: React.FormEvent<HTMLInputElement>)=>{
+//    const{
+//     currentTarget: {value},
 
-   } = event;
-   setUsername(value);
+//    } = event;
+//    setUsername(value);
 
-  };
- const onSubmit = (event: React.FormEvent<HTMLFormElement>)=>{
-  event.preventDefault();
-  console.log("hello", username)
- };
+//   };
+//  const onSubmit = (event: React.FormEvent<HTMLFormElement>)=>{
+//   event.preventDefault();
+//   console.log("hello", username)
+//  };
 
- const onClick = (event: React.FormEvent<HTMLButtonElement>)=>{
-  event.preventDefault();
-  console.log("hi")
- }
+//  const onClick = (event: React.FormEvent<HTMLButtonElement>)=>{
+//   event.preventDefault();
+//   console.log("hi")
+//  }
 
-  return <>
-    <form onSubmit={onSubmit}>
-      <input type="text" placeholder="username" value={username} onChange={onChange}/>
-      <button>Log in</button>
-      <Dummy text="hello"></Dummy>
-      <button onClick={onClick}> Click </button>
-    </form>
+  return <div>
+    <Router/>
    
-    </>
+    </div>
 }
 
 export default App;
---
+++ react-master/src/Router.tsx
@@ -0,0 +1,15 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./screens/Home";
+import About from "./screens/About";
+function Router(){
+    return <BrowserRouter>
+    <Header/>
+     <Routes>
+        <Route path="/" element={<Home></Home>}/>
+        <Route path="/about" element={<About></About>}/>
+     </Routes>
+    </BrowserRouter>
+}
+
+export default Router;
